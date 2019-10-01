565914db87ef70f14abcb9fcb33f0fa15d5a2c15
Wesley Araujo <wesley.sickert@gmail.com>
2019-10-01 09:37:01 -0300

    add sign canvas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,75 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Document, Page, pdfjs } from 'react-pdf'
-// import myPDF from 'file.pdf'
+import { Button, Modal } from 'react-bootstrap'
+import SignatureCanvas from 'react-signature-canvas'
+import { PDFDocument } from 'pdf-lib'
+
+async function modifyPDF(pdf) {
+  const reader = new FileReader()
+  // const unint = new Uint8Array(pdf)
+  reader.readAsArrayBuffer(pdf)
+  reader.onload = async () => {
+    console.log(reader.result)
+    const pdfDoc = await PDFDocument.load(reader.result)
+    const pages = pdfDoc.getPages()
+    console.log(pages)
+    pages[0].drawText('You can modify PDFs too!')
+    const pdfBytes = await pdfDoc.save()
+    console.log(pdfBytes)
+    const blob = new Blob(pdfBytes, {type:'application/pdf'})
+    console.log(blob)
+  }
+}
+
+const SignatureModal = (props) => {
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  const handleSave = (ref) => {
+    ref.toDataURL()
+  }
+
+  const sigPad = (ref) => {
+
+  }
+  
+  var algo = {}
+
+  return(
+    (
+  
+      <>
+        <Button variant="primary" onClick={handleShow}>
+            Criar Assinatura
+          </Button>
+    
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Criar Assinatura</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <SignatureCanvas 
+                ref={ (ref) => sigPad(ref) }
+                canvasProps={{
+                  width: 500,
+                  height: 200
+                }
+              } />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Fechar
+              </Button>
+              <Button variant="primary" onClick={handleSave()}>
+                Salvar
+              </Button>
+            </Modal.Footer>
+        </Modal>
+      </>
+    )
+  )
+}
 
 function App() {
 
@@ -22,6 +90,7 @@ function App() {
       <input type="file" name="file" accept="application/pdf" onChange={ (event) => {
         console.log(event.target.files[0])
         setFile(event.target.files[0])
+        modifyPDF(event.target.files[0])
         } }/>
       <nav>
         <button onClick={goToPrevPage}>Anterior</button>
@@ -36,8 +105,8 @@ function App() {
           <Page pageNumber={pageNumber} />
         </Document>
       </div>
-
       {/* Página {pageNumber} de {numPages} */}
+      <SignatureModal />
     </div>
   );
 }
